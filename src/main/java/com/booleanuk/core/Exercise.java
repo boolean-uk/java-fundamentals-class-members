4b826ada1b252be9599a2082d8845e465c0d8a33
Rose Breedveld <rosebreedveld@hotmail.com>
2023-04-26 10:05:02 -0400

    standard 4 - 6

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -26,15 +26,13 @@ public class Exercise extends ExerciseBase {
 
     // 4. Change the value below so that the tests pass. Check the README.md file for instructions on
     // running and reading tests
-    public int firstNumber = 109;
-
+    public int firstNumber = 9182;
 
 
     // 5. Change the value below so that the tests pass
-    public String firstString = "Ruby is to Python what car is to carpet.";
-
+    public String firstString = "Java is to Javascript what car is to carpet.";
 
 
     // 6. Change the visibility below so that the tests pass
-    private boolean isVisible = true;
+    public boolean isVisible = true;
 }
