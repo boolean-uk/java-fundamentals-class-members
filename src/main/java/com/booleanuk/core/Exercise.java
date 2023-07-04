f50812e108af108e3c87dfd78b71a4698e32dde1
Rafal Halama <rafau.halama@gmail.com>
2023-07-04 14:06:05 +0200

    first 3 tests

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -14,15 +14,15 @@ public class Exercise extends ExerciseBase {
 
     // 1. Create an integer member named age with a value of 32
 
-
+    public int age = 32;
 
     // 2. Create a String member named firstName with a value of "Jane"
 
-
+    public String firstName = "Jane";
 
     // 3. Create a boolean member named isProgrammer with a value of true
 
-
+    public boolean isProgrammer = true;
 
     // 4. Change the value below so that the tests pass. Check the README.md file for instructions on
     // running and reading tests
