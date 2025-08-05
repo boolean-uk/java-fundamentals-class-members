bd22d6686a3397c15de08c2c1da9568d3488cf68
Roderick Leito <roderick.leito@gmail.com>
2025-08-05 13:36:17 +0200

    Finished

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -12,11 +12,11 @@ public class Exercise extends ExerciseBase {
     public int answerToTheUltimateQuestion = 42;
 
     // 1. Create an integer member named age with a value of 32
-    int age = 32;
+    public int age = 32;
 
 
     // 2. Create a String member named firstName with a value of "Jane"
-    String firstName = "Jane";
+    public String firstName = "Jane";
 
 
     // 3. Create a boolean member named isProgrammer with a value of true
@@ -28,11 +28,10 @@ public class Exercise extends ExerciseBase {
     public int firstNumber = 9182;
 
 
-
     // 5. Change the value below so that the tests pass
     public String firstString = "Java is to Javascript what car is to carpet.";
 
 
-
     // 6. Change the visibility below so that the tests pass
     public boolean isVisible = true;
+}
